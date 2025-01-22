85123b16ab9b5868036c684f7a4866f7ef177a28
AdrianGerman <adrian.german1019@gmail.com>
2025-01-22 08:17:12 -0600

    create data file for info webs

---
+++ src/data/designData.js
@@ -0,0 +1,31 @@
+export const designResources = [
+  {
+    name: "Flowbite",
+    url: "https://flowbite.com/",
+    description: "Componentes para la mayoría de frameworks con TailwindCSS."
+  },
+  {
+    name: "Tabler",
+    url: "https://tabler.io/icons",
+    description:
+      "Biblioteca de iconos svg para cualquier web, con opciones de configuración."
+  },
+  {
+    name: "Heroicons",
+    url: "https://heroicons.com/",
+    description:
+      "Biblioteca de iconos pensado en proyectos React, con mas extensión de iconos que Tabler pero menos configuración."
+  },
+  {
+    name: "Ibelick",
+    url: "http://bg.ibelick.com/",
+    description:
+      "Diseños de fondo para tus webs, funciona con TailwindCSS en cualquier proyecto"
+  },
+  {
+    name: "svgl",
+    url: "https://svgl.app/",
+    description:
+      "Galería de iconos svg de marcas, a color y con una gran extensión de iconos."
+  }
+]
