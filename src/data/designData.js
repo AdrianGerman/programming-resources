a5d65f58b7765eee98e838db9b6d00bb1edff01f
AdrianGerman <adrian.german1019@gmail.com>
2025-01-23 08:49:55 -0600

    more webs data

--- src/data/designData.js
+++ src/data/designData.js
@@ -5,6 +5,12 @@ export const designResources = [
     description: "Componentes para la mayoría de frameworks con TailwindCSS."
   },
   {
+    name: "HyperUI",
+    url: "https://www.hyperui.dev/",
+    description:
+      "Catalogo de componentes para HTML, React y Vue creados con TailwindCSS"
+  },
+  {
     name: "Tabler",
     url: "https://tabler.io/icons",
     description:
@@ -27,5 +33,27 @@ export const designResources = [
     url: "https://svgl.app/",
     description:
       "Galería de iconos svg de marcas, a color y con una gran extensión de iconos."
+  },
+  {
+    name: "Adobe Color",
+    url: "https://color.adobe.com/es/create/color-wheel",
+    description: "Herramienta de adobe gratuita para generar paletas de colores"
+  },
+  {
+    name: "ColorSpace",
+    url: "https://mycolor.space/",
+    description:
+      "Generador de paletas de colores y degradados en base a un color hexadecimal proporcionado por el usuario."
+  },
+  {
+    name: "",
+    url: "https://colorhunt.co/",
+    description:
+      "Galería de paletas de colores, en este caso las paletas ya están predefinidas y no pueden ser editadas o modificadas."
+  },
+  {
+    name: "Neumorphism.io",
+    url: "https://neumorphism.io/",
+    description: "Generador de sombras para componentes web."
   }
 ]
--- src/data/imageData.js
+++ src/data/imageData.js
@@ -12,6 +12,12 @@ export const imageResources = [
       "Compresor de imágenes para la optimización web, la diferencia con 'Squoosh' es que aquí te permite cargar multiples imágenes en una sola carga."
   },
   {
+    name: "Tinypng",
+    url: "https://tinypng.com/",
+    description:
+      "Compresión inteligente de imágenes, la diferencia con el resto es que es un poco mas profesional, en mi gusto no la suelo usar."
+  },
+  {
     name: "Shots",
     url: "https://shots.so/",
     description:
@@ -24,6 +30,18 @@ export const imageResources = [
       "Biblioteca de imágenes gratuitas y libres de derechos de autor."
   },
   {
+    name: "Pixabay",
+    url: "https://pixabay.com/es/",
+    description:
+      "Biblioteca de imágenes gratuitas y libres de derecho de autor. a mi gusto mejor que Pexels por un mayor catalogo."
+  },
+  {
+    name: "Unsplash",
+    url: "https://unsplash.com/es",
+    description:
+      "Galería de imágenes gratuitas y libres de derechos de autor, esta galería suele actualizarse constantemente."
+  },
+  {
     name: "Photopea",
     url: "https://www.photopea.com/",
     description: "Editor de imágenes estilo Photoshop pero web y gratuito."
@@ -33,5 +51,34 @@ export const imageResources = [
     url: "https://blurred.dev/",
     description:
       "Difuminar imágenes, convierte una imagen en una más pequeña y borrosa para mostrarla en la carga o en demás secciones."
+  },
+  {
+    name: "Imgur",
+    url: "https://imgur.com/",
+    description:
+      "Web para subir tus imágenes y posteriormente poderlas usar en tus webs con url."
+  },
+  {
+    name: "Postimages",
+    url: "https://postimages.org/es/",
+    description:
+      "Web para subir imágenes, conseguir enlaces permanentes o temporales para nuestras webs."
+  },
+  {
+    name: "Digital inspiration",
+    url: "https://www.labnol.org/embed/google/drive/",
+    description:
+      "Genera enlaces de imágenes de google drive, para usarlos en tus paginas web."
+  },
+  {
+    name: "Lorem Picsum",
+    url: "https://picsum.photos/",
+    description:
+      "Generador de imágenes aleatorias, para el uso web y practicas de diseño."
+  },
+  {
+    name: "Base64 image",
+    url: "https://www.base64-image.de/",
+    description: "Convierte tus imágenes en base 64."
   }
 ]
