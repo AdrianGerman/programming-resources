ca79f17578f6726573da006f8094247d779965df
AdrianGerman <adrian.german1019@gmail.com>
2025-01-23 08:00:12 -0600

    fonts web data

---
+++ src/data/FontsData.js
@@ -0,0 +1,20 @@
+export const fontsResources = [
+  {
+    name: "Google Fonts",
+    url: "https://fonts.google.com/",
+    description:
+      "El servicio de Google, solo debes importar en el HTML su referencia y podrás usar tus fuentes preferidas."
+  },
+  {
+    name: "Fontsource",
+    url: "https://fontsource.org/",
+    description:
+      "A diferencia de Google Fonts, esta web esta pensada para Frameworks ya que las fuentes se instalan como un paquete npm."
+  },
+  {
+    name: "Dafont",
+    url: "https://www.dafont.com/es/",
+    description:
+      "Si lo que buscas es una fuente muy especifica esta es tu web, no esta pensada para web, asi que tendrás que descargar la fuente y importarla de manera manual, la extensión de esta web es 4 veces mayor que las anteriores opciones."
+  }
+]
