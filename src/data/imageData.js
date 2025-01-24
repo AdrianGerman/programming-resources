9f4661a8be00aff2e0b4a9bcc155f5721572c943
AdrianGerman <adrian.german1019@gmail.com>
2025-01-24 10:06:04 -0600

    add images

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -2,83 +2,97 @@ export const imageResources = [
   {
     name: "Squoosh",
     url: "https://squoosh.app/",
+    img: "/public/image/squoosh.webp",
     description:
       "Compresor y convertidor de imágenes para la optimización en el uso de paginas web."
   },
   {
     name: "imgto.xyz",
     url: "https://imgto.xyz/",
+    img: "/public/image/imgto.webp",
     description:
       "Compresor de imágenes para la optimización web, la diferencia con 'Squoosh' es que aquí te permite cargar multiples imágenes en una sola carga."
   },
   {
     name: "Tinypng",
     url: "https://tinypng.com/",
+    img: "/public/image/tinypng.webp",
     description:
       "Compresión inteligente de imágenes, la diferencia con el resto es que es un poco mas profesional, en mi gusto no la suelo usar."
   },
   {
     name: "Shots",
     url: "https://shots.so/",
+    img: "/public/image/shots.webp",
     description:
       "Creación de imágenes, para la presentación de las mismas en tu pagina web."
   },
   {
     name: "Pexels",
     url: "https://www.pexels.com/es-es/",
+    img: "/public/image/pexels.webp",
     description:
       "Biblioteca de imágenes gratuitas y libres de derechos de autor."
   },
   {
     name: "Pixabay",
     url: "https://pixabay.com/es/",
+    img: "/public/image/pixabay.webp",
     description:
       "Biblioteca de imágenes gratuitas y libres de derecho de autor. a mi gusto mejor que Pexels por un mayor catalogo."
   },
   {
     name: "Unsplash",
     url: "https://unsplash.com/es",
+    img: "/public/image/unsplash.webp",
     description:
       "Galería de imágenes gratuitas y libres de derechos de autor, esta galería suele actualizarse constantemente."
   },
   {
     name: "Photopea",
     url: "https://www.photopea.com/",
+    img: "/public/image/photopea.webp",
     description: "Editor de imágenes estilo Photoshop pero web y gratuito."
   },
   {
     name: "Blurred",
     url: "https://blurred.dev/",
+    img: "/public/image/blurred.webp",
     description:
       "Difuminar imágenes, convierte una imagen en una más pequeña y borrosa para mostrarla en la carga o en demás secciones."
   },
   {
     name: "Imgur",
     url: "https://imgur.com/",
+    img: "/public/image/imgur.webp",
     description:
       "Web para subir tus imágenes y posteriormente poderlas usar en tus webs con url."
   },
   {
     name: "Postimages",
     url: "https://postimages.org/es/",
+    img: "/public/image/postimages.webp",
     description:
       "Web para subir imágenes, conseguir enlaces permanentes o temporales para nuestras webs."
   },
   {
     name: "Digital inspiration",
     url: "https://www.labnol.org/embed/google/drive/",
+    img: "/public/image/digital.webp",
     description:
       "Genera enlaces de imágenes de google drive, para usarlos en tus paginas web."
   },
   {
     name: "Lorem Picsum",
     url: "https://picsum.photos/",
+    img: "/public/image/picsum.webp",
     description:
       "Generador de imágenes aleatorias, para el uso web y practicas de diseño."
   },
   {
     name: "Base64 image",
     url: "https://www.base64-image.de/",
+    img: "/public/image/base64.webp",
     description: "Convierte tus imágenes en base 64."
   }
 ]
