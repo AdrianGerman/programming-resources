22d5b13558c839dc373d5ed0508fb79b3c8762b8
AdrianGerman <adrian.german1019@gmail.com>
2025-01-24 08:20:19 -0600

    web component and web data resources

---
+++ src/data/webData.js
@@ -0,0 +1,14 @@
+export const webResources = [
+  {
+    name: "Carbon",
+    url: "https://carbon.now.sh/",
+    description:
+      "Web para compartir nuestros bloques de código, para que visualmente se vea lindo."
+  },
+  {
+    name: "Mydevice",
+    url: "https://www.mydevice.io/",
+    description:
+      "Web que te muestra el tamaño de tu pantalla o de la ventana donde esta la web, esto para algunos temas relacionados con los diseños."
+  }
+]
