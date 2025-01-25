756a5313c46306ab8e4b016e9bd399a8d2b0d847
AdrianGerman <adrian.german1019@gmail.com>
2025-01-25 08:44:44 -0600

    apply new styles in design component and fix url image

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -2,62 +2,62 @@ export const imageResources = [
   {
     name: "Squoosh",
     url: "https://squoosh.app/",
-    img: "/public/image/squoosh.webp",
+    img: "/image/squoosh.webp",
     description:
       "Compresor y convertidor de imágenes para la optimización en el uso de paginas web."
   },
   {
     name: "imgto.xyz",
     url: "https://imgto.xyz/",
-    img: "/public/image/imgto.webp",
+    img: "/image/imgto.webp",
     description:
       "Compresor de imágenes para la optimización web, la diferencia con 'Squoosh' es que aquí te permite cargar multiples imágenes en una sola carga."
   },
   {
     name: "Tinypng",
     url: "https://tinypng.com/",
-    img: "/public/image/tinypng.webp",
+    img: "/image/tinypng.webp",
     description:
       "Compresión inteligente de imágenes, la diferencia con el resto es que es un poco mas profesional, en mi gusto no la suelo usar."
   },
   {
     name: "Shots",
     url: "https://shots.so/",
-    img: "/public/image/shots.webp",
+    img: "/image/shots.webp",
     description:
       "Creación de imágenes, para la presentación de las mismas en tu pagina web."
   },
   {
     name: "Pexels",
     url: "https://www.pexels.com/es-es/",
-    img: "/public/image/pexels.webp",
+    img: "/image/pexels.webp",
     description:
       "Biblioteca de imágenes gratuitas y libres de derechos de autor."
   },
   {
     name: "Pixabay",
     url: "https://pixabay.com/es/",
-    img: "/public/image/pixabay.webp",
+    img: "/image/pixabay.webp",
     description:
       "Biblioteca de imágenes gratuitas y libres de derecho de autor. a mi gusto mejor que Pexels por un mayor catalogo."
   },
   {
     name: "Unsplash",
     url: "https://unsplash.com/es",
-    img: "/public/image/unsplash.webp",
+    img: "/image/unsplash.webp",
     description:
       "Galería de imágenes gratuitas y libres de derechos de autor, esta galería suele actualizarse constantemente."
   },
   {
     name: "Photopea",
     url: "https://www.photopea.com/",
-    img: "/public/image/photopea.webp",
+    img: "/image/photopea.webp",
     description: "Editor de imágenes estilo Photoshop pero web y gratuito."
   },
   {
     name: "Blurred",
     url: "https://blurred.dev/",
-    img: "/public/image/blurred.webp",
+    img: "/image/blurred.webp",
     description:
       "Difuminar imágenes, convierte una imagen en una más pequeña y borrosa para mostrarla en la carga o en demás secciones."
   },
