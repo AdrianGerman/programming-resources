33891af7f0b97b018f37d82000cdcee611c36e59
AdrianGerman <adrian.german1019@gmail.com>
2025-01-25 08:44:02 -0600

    add design images

--- src/data/designData.js
+++ src/data/designData.js
@@ -2,58 +2,68 @@ export const designResources = [
   {
     name: "Flowbite",
     url: "https://flowbite.com/",
+    img: "/design/flowbite.webp",
     description: "Componentes para la mayoría de frameworks con TailwindCSS."
   },
   {
     name: "HyperUI",
     url: "https://www.hyperui.dev/",
+    img: "/design/hyperui.webp",
     description:
       "Catalogo de componentes para HTML, React y Vue creados con TailwindCSS"
   },
   {
     name: "Tabler",
     url: "https://tabler.io/icons",
+    img: "/design/tabler.webp",
     description:
       "Biblioteca de iconos svg para cualquier web, con opciones de configuración."
   },
   {
     name: "Heroicons",
     url: "https://heroicons.com/",
+    img: "/design/heroicons.webp",
     description:
       "Biblioteca de iconos pensado en proyectos React, con mas extensión de iconos que Tabler pero menos configuración."
   },
   {
     name: "Ibelick",
     url: "http://bg.ibelick.com/",
+    img: "/design/ibelick.webp",
     description:
       "Diseños de fondo para tus webs, funciona con TailwindCSS en cualquier proyecto"
   },
   {
     name: "svgl",
     url: "https://svgl.app/",
+    img: "/design/svgl.webp",
     description:
       "Galería de iconos svg de marcas, a color y con una gran extensión de iconos."
   },
   {
     name: "Adobe Color",
     url: "https://color.adobe.com/es/create/color-wheel",
+    img: "/design/adobe-color.webp",
     description: "Herramienta de adobe gratuita para generar paletas de colores"
   },
   {
     name: "ColorSpace",
     url: "https://mycolor.space/",
+    img: "/design/color-space.webp",
     description:
       "Generador de paletas de colores y degradados en base a un color hexadecimal proporcionado por el usuario."
   },
   {
-    name: "",
+    name: "Color Hunt",
     url: "https://colorhunt.co/",
+    img: "/design/colorhunt.webp",
     description:
       "Galería de paletas de colores, en este caso las paletas ya están predefinidas y no pueden ser editadas o modificadas."
   },
   {
     name: "Neumorphism.io",
     url: "https://neumorphism.io/",
+    img: "/design/neumorphism.webp",
     description: "Generador de sombras para componentes web."
   }
 ]
