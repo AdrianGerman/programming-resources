9a501f752b4e357ce7d25de8f8f4b1704bb6e68d
AdrianGerman <adrian.german1019@gmail.com>
2025-01-26 08:37:35 -0600

    add images web and web data

--- src/data/webData.js
+++ src/data/webData.js
@@ -2,12 +2,14 @@ export const webResources = [
   {
     name: "Carbon",
     url: "https://carbon.now.sh/",
+    img: "/webs/carbon.webp",
     description:
       "Web para compartir nuestros bloques de código, para que visualmente se vea lindo."
   },
   {
     name: "Mydevice",
     url: "https://www.mydevice.io/",
+    img: "/webs/mydevice.webp",
     description:
       "Web que te muestra el tamaño de tu pantalla o de la ventana donde esta la web, esto para algunos temas relacionados con los diseños."
   }
